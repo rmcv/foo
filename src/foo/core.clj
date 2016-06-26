2e3e13da9121662fe1feddcccd8993e910a40730
Rimon <rimonchi@gmail.com>
2016-06-26 19:08:54 +0800

    fix

--- src/foo/core.clj
+++ src/foo/core.clj
@@ -24,56 +24,37 @@
 
 (defn equi [xs]
   (let [rs  (reduce +' xs)
-        ans (loop [[a & xs] xs
-                   i        0
-                   rs       rs
-                   ls       0]
-              (when a
-                (if (not= rs ls)
-                  (recur xs (inc i) (-' rs a) (+' ls a))
-                  i)))]
-    (or ans -1))
-  #_(or (let [ys (map-indexed (fn [i e] [i  (sum-eq? xs e)]) (range 0 (count xs)))]
-        (->> ys
-             (filter (comp true? last))
-             ffirst))
-      -1)
-  #_(rand-int (count xs)))
+        ans (loop [xs xs
+                   i  0
+                   rs rs
+                   ls 0]
+              (let [[a & xs] xs
+                    rs (-' rs (or a 0))]
+                (if (= rs ls)
+                  i
+                  (when (seq xs)
+                    (recur xs (inc i) rs (+' ls a))))))]
+    (or ans -1)))
 
 (defn sum-eq? [xs i]
-  (let [[ls rs] (split-at i xs)]
+  (let [[ls [_ & rs]] (split-at i xs)]
     (= (reduce +' ls) (reduce +' rs))))
 
 (s/fdef equi
         :args (s/cat :xs (s/coll-of int? []))
         :ret (s/or
-              :no-soln #(= % -1)
+              :no-soln  #(= % -1)
               :has-soln #(>= % 0))
         :fn #(let [[rt v] (:ret %)]
                (case rt
-                 :no-soln (not-any? (partial sum-eq? (-> % :args :xs)) (range 0 (-> % :args :xs count))) 
+                 :no-soln  (not-any? (partial sum-eq? (-> % :args :xs)) (range 0 (-> % :args :xs count)))
                  :has-soln (let [xs (-> % :args :xs)
                                  ls (reduce +' (take v xs))
-                                 rs (reduce +' (drop v xs))]
-                             (= ls rs))))
-        #_:fn #_(s/or
-             :no-soln (s/and
-                       #(= -1 (:ret %))
-                       #_(not-any? (partial sum-eq? (-> % :args :xs)) (range 0 (-> % :args :xs count))))
-             :yes-soln (s/and
-                        #(>= (:ret %) 0)
-                        #(<= (:ret %) (-> :args :xs count))
-                        #_(let [xs (-> % :args :xs)
-                               ret (:ret %)
-                               ls (reduce +' (take ret xs))
-                               rs (reduce +' (drop ret xs))]
-                           (= ls rs)))))
+                                 rs (reduce +' (drop (inc v) xs))]
+                             (= ls rs)))))
 
 
 (st/check-var #'equi)
-
-
-;; changing
 
 
 (s/def ::S (s/coll-of #{1 -1} []))
@@ -88,9 +69,3 @@
 (gen/sample (s/gen ::N))
 
 (s/explain ::S [1 1 1 -1 -2])
-
-(s/fdef min-abs-sum
-        :args (s/cat :A ::A)
-        :ret int?
-        :fn 
-        )
